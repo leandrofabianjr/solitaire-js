fd6f9ef04f15a88e22ba1799dc508202c6b0f74f
leandrofabianjr <leandrofabianjr@gmail.com>
2019-11-18 18:05:40 -0300

    Tratado validação de evento de arraste das cartas

--- js/card.js
+++ js/card.js
@@ -27,7 +27,30 @@ class Card {
         this._$card.classList.add('card', this.suit);
         this._$card.draggable = true;
         this._$card.id = `${this.suit}-${this.number}`;
-        this._$card.ondragstart = (ev) => ev.dataTransfer.setData('card', ev.target.id);
+        this._$card.dataset.suit = this.suit;
+        this._$card.dataset.number = this.number;
+        this._$card.ondragstart = (ev) => {
+            const $c = ev.target.closest('.card');
+            const $parent = $c.parentElement;
+            const $siblings = Array.from($parent.children);
+            const cardIndex = $siblings.indexOf($c);
+            if ($parent.classList.contains('deck') && $siblings.length > 1 && cardIndex + 1 !== $siblings.length) {
+                console.log($siblings, cardIndex);
+                const $bottomSiblings = $siblings.slice(cardIndex + 1);
+                console.log($bottomSiblings);
+                let $lastSibling = $c;
+                for (let i = 0; i < $bottomSiblings.length; i++) {
+                    if (+$bottomSiblings[i].dataset.number + 1 !== +$lastSibling.dataset.number) {
+                        return false;
+                    }
+                    if (['spades', 'clubs'].includes($lastSibling.dataset.suit) && ['spades', 'clubs'].includes($bottomSiblings[i].dataset.suit)) {
+                        return false;
+                    }
+                    $lastSibling = $bottomSiblings[i];
+                }
+            }
+            ev.dataTransfer.setData('cardId', ev.target.id);
+        }
         this._$card.ondrop = () => false;
 
         this._$card.innerHTML = `
@@ -53,5 +76,9 @@ class Card {
             default:
                 return this.number;
         }
+    }
+
+    turn() {
+        this.$card.classList.add('back');
     }
 }
